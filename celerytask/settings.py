48d4917047a140d1463ca2933950591c86e5a4e8
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-08-30 10:16:13 +0000

    Url and Requirement

--- celerytask/settings.py
+++ celerytask/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'celerytask.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # We are adding in the directory where our templates will be stored.
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- celerytask/urls.py
+++ celerytask/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task.views import GenerateRandomUserView, UsersListView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', UsersListView.as_view(), name='users_list'),
+    path('generate/', GenerateRandomUserView.as_view(), name='generate')
 ]
